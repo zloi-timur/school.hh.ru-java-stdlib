60705636a38c772b4882fe2ef530dcdd51cbc579
Timur Valiev <zloi.timur@gmail.com>
2011-12-21 14:06:07 +0400

    11

--- src/main/java/ru/hh/school/stdlib/Server.java
+++ src/main/java/ru/hh/school/stdlib/Server.java
@@ -2,17 +2,51 @@ package ru.hh.school.stdlib;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Executors.;
 
 public class Server {
-  public Server(InetSocketAddress addr) {
-    throw new UnsupportedOperationException();
-  }
+    private ServerSocket server;
+    private Substitutor3000 substitutor;
+    private Integer sleepTime;
+    public Server(InetSocketAddress addr) {
+        try {
+            server = new ServerSocket(addr.getPort(),0,addr.getAddress());
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        substitutor = new Substitutor3000();
+        this.sleepTime = 0;
+    }
 
-  public void run() throws IOException {
-    throw new UnsupportedOperationException();
-  }
+    public Server(InetSocketAddress addr, Integer sleepTime) {
+        try {
+            server = new ServerSocket(addr.getPort(),0,addr.getAddress());
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        substitutor = new Substitutor3000();
+        this.sleepTime = sleepTime;
+    }
 
-  public int getPort() {
-    throw new UnsupportedOperationException();
-  }
+    public void run() throws IOException {
+        Executor executor = new Executors.;
+        for (;;){
+            Socket clientSocket = server.accept();
+            Runnable client = new Runnable() {
+                @Override
+                public void run() {
+                    //To change body of implemented methods use File | Settings | File Templates.
+                }
+            };
+
+        }
+    }
+
+    public int getPort() {
+        return server.getLocalPort();
+    }
 }
